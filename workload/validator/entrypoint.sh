0576dd1b765e7565daf416adc4c350f2fd517bf4
Chang Xiao <chang.xiao@antithesis.com>
2025-06-17 16:42:54 -0400

    adding healthcheck

---
+++ workload/validator/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ANTITHESIS_OUTPUT_DIR=${ANTITHESIS_OUTPUT_DIR:-"/tmp"}
+
+echo '{\"antithesis_setup\": { \"status\": \"complete\", \"details\": null }}' > $ANTITHESIS_OUTPUT_DIR/sdk.jsonl
+
+sleep infinity
