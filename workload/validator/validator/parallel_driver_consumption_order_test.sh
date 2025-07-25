6aa5a542a86989947de4e90c82d007331ca72b18
eric-dinh-antithesis <eric.dinh@antithesis.com>
2025-07-25 14:11:02 -0400

    move test composer files to their own directory, update dockerfile to support

--- workload/validator/validator/parallel_driver_consumption_order_test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-RUST_LOG=error /validator/target/debug/validator --test consumption-order
--- workload/validator/validator/parallel_driver_exactly_once_guarantee_test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-RUST_LOG=error /validator/target/debug/validator --test exactly-once-guarantee
